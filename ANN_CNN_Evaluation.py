716b909e3253471fd71182cc6b33f0f8111fe882
Spasov <51121651+enerhy@users.noreply.github.com>
2020-01-07 16:59:01 +0200

    Update ANN_CNN_Evaluation.py

--- ANN_CNN_Evaluation.py
+++ ANN_CNN_Evaluation.py
@@ -1,8 +1,11 @@
 ---------TF2
-# Evaluating a binary classification with ROC
+# Evaluating a binary classification with ROC and Accuracy
 
 def auroc(y_true, y_pred):
     return tf.py_func(roc_auc_score, (y_true, y_pred), tf.double)
 
 # Build Model...
 model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy', auroc])
+
+r = model.fit(.....)
+print(r.history['accuracy], 'accuracy)
